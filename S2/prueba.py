82d390caaa8cc800a877bbd06a8bc365c1d76873
jmorales <jonathan.morales.jmr@gmail.com>
2023-06-26 16:31:29 -0400

    act

---
+++ S2/prueba.py
@@ -0,0 +1,24 @@
+"""
+python -V
+pip -V
+pip install virtualenvwrapper
+pip install virtualenv
+virtualenv {algun_nombre} --->> este comando no me sirvio
+python -m virtualenv django_env
+.\django_env\Scripts\activate.ps1
+Get-ExecutionPolicy
+Set-ExecutionPolicy RemoteSigned
+"""
+
+
+"""
+Listar lista de paquetes instalados 
+pip list
+
+comandos de ayuda pip
+pip help
+
+pip install django
+python -m django --version
+
+"""
